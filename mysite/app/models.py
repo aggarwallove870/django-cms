bb50beafb89ba872c1553711bc99ab8d3222f22e
vijay-snakescript <vijaysharma@snakescript.com>
2023-01-09 18:30:50 +0530

    center

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -46,6 +46,8 @@ class FlexPage(Page):
         [
             ("title_and_text" , blocks.TitleAndTextBlock()),
             ("richtext", blocks.RichTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
             ("card",blocks.CardBlock()),
             ("fourdiv", blocks.FourdivBlock()),
             ("footer", blocks.FooterBlock()),
